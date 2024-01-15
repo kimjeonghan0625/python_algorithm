67a14f07e3a72c12f762b1d1b71c50f2fc0c94c7
jhkim <mineship@pusan.ac.kr>
2024-01-15 20:36:22 +0900

    add remove method on linked_list.py

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -155,10 +155,26 @@ class LinkedList:
         # self.length += 1
         # return True
 
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            self.pop_first()
+        if index == self.length - 1:
+            self.pop()
+        pre = self.get(index - 1)
+        temp = pre.next
+        pre.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
 
-my_linked_list = LinkedList(0)
-my_linked_list.append(2)
 
-my_linked_list.insert(1, 1)
+my_linked_list = LinkedList(11)
+my_linked_list.append(3)
+my_linked_list.append(23)
+my_linked_list.append(7)
+
+print(my_linked_list.remove(2).value, "\n")
 
 my_linked_list.print_list()
